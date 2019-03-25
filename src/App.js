0b3f0d8fccd55b8bf0f3468b17ead2675d773ab2
Samuli Löf <samuli.lof@gmail.com>
2019-03-25 15:44:26 +0200

    login and logout done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
+  useEffect(() => {
+    const user = window.localStorage.getItem('BlogUser')
+    if (user) setUser(JSON.parse(user))
+  }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm setUser={setUser} />
+      </div>
+    )
+  }
+  const logOut = () => {
+    window.localStorage.removeItem('BlogUser')
+    setUser(null)
+  }
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      <div>
+        <button onClick={logOut}>Logout</button>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ src/components/LoginForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import loginService from '../services/login'
+
+const LoginForm = (props) => {
+  const { setUser } = props
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitForm = async e => {
+    e.preventDefault()
+    const user = await loginService.login({ username, password })
+    setUsername('')
+    setPassword('')
+
+    setUser(user)
+    window.localStorage.setItem('BlogUser', JSON.stringify(user))
+  }
+  return (
+    <form>
+      <div>
+        käyttäjätunnus:
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        salasana:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={submitForm}>Kirjaudu</button>
+      </div>
+    </form>
+  )
+}
+
+export default LoginForm
